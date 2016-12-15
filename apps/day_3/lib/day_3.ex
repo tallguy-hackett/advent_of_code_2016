30f26a69757634326148569016412b433316281d
Dave Hackett <tallguy.hackett@gmail.com>
2016-12-14 22:09:06 -0700

    Add part 2 to Day 3

--- apps/day_3/lib/day_3.ex
+++ apps/day_3/lib/day_3.ex
@@ -17,4 +17,43 @@ defmodule Day3 do
   end
 
   def possible_triangle?([a, b, c]), do: a + b > c
+
+
+
+  def part_2(filename) do
+    filename
+    |> File.read!()
+    |> String.split("\r\n")
+    |> parse_columns([])
+    |> Enum.map(&part_2_triangle?/1)
+    |> Enum.count(&(&1))
+    |> IO.puts
+  end
+
+  def part_2_triangle?(line) do
+    line
+    |> Enum.sort()
+    |> possible_triangle?()
+  end
+
+  def parse_columns([], acc), do: acc
+  def parse_columns(file_contents, acc) do
+    parsed_triangles = file_contents
+    |> Enum.take(3)
+    |> Enum.map(&String.trim/1)
+    |> parse_triangles()
+    parse_columns(Enum.drop(file_contents, 3), acc++parsed_triangles)
+  end
+
+  def parse_triangles([row_one, row_two, row_three]) do
+    [a1, a2, a3] = String.split(row_one, " ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    [b1, b2, b3] = String.split(row_two, " ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    [c1, c2, c3] = String.split(row_three, " ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    [[a1, b1, c1],
+      [a2, b2, c2],
+      [a3, b3, c3]]
+  end
 end
--- apps/day_3/test/day_3_test.exs
+++ apps/day_3/test/day_3_test.exs
@@ -17,4 +17,12 @@ defmodule Day3Test do
   test "possible_triangle? returns true for [5, 10, 14]" do
     assert Day3.possible_triangle?([5,10,14]) == true
   end
+
+  test "parse_columns returns [[1,1,1],[2,2,2],[3,3,3]] for ['1 2 3', '1 2 3 ', '1 2 3']" do
+    assert Day3.parse_columns(["1 2 3", "1 2 3 ", "1 2 3"], []) == [[1,1,1],[2,2,2],[3,3,3]]
+  end
+
+  test "parse_columns returns [[1,1,1],[2,2,2],[3,3,3]] for ['101 301 501','102 302 502','103 303 503','201 401 601','202 402 602','203 403 603']" do
+    assert Day3.parse_columns(["101 301 501","102 302 502","103 303 503","201 401 601","202 402 602","203 403 603"], []) == [[101,102,103],[301,302,303],[501,502,503],[201,202,203],[401,402,403],[601,602,603]]
+  end
 end
